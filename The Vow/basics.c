4bf6e4ffc6f5f38421a342d8b9c5655da60c1bc7
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-16 19:14:41 +0530

    feat: add C syntax recap and bitwise operator practice

---
+++ The Vow/basics.c
@@ -0,0 +1,46 @@
+// Rough works and remembering the C syntax
+/*
+#include <stdio.h>
+int main(){
+int a = 10;
+float b = 23;
+char name = 'S';
+printf("a + b = %f \n",a+b);
+printf("%c \n",name);
+printf("The size of int() is %lu \n",sizeof(int));
+printf("The size of float() is %lu \n",sizeof(float));
+printf("The size of char() is %lu \n",sizeof(char));
+printf("The size of long() is %lu \n",sizeof(long));
+printf("The size of unsigned int() is %lu \n",sizeof(unsigned int));
+int shanviBeta;
+printf("Your age - 2 = %lu \n\n",sizeof(shanviBeta));
+printf("******* Recapng Of Arithmatic Operater *******\n\n");
+int x =4, y = 54, z =33;
+printf("The addition = %d \n",x+y+z); // Addition
+printf("The subtraction = %d \n",x-y-z); // Subtraction
+printf("The multiplication = %d \n\n",x*y*z); //multiplication
+printf("******* Recapng Of logical Operator *******\n\n");
+int num1 = 0;
+int num2 =1;
+int ShanviIsAGoodGirl = 1;
+printf("Recaping the 'OR' operetor %d\n",num1||num2);
+printf("Recaping the 'NOT' operetor %d\n",!num1);
+printf("Recaping the 'AND' operetor %d\n",num1&&num2);
+printf("Shanvi is not a good girl ? %d\n\n", !ShanviIsAGoodGirl);
+printf("******* Recapng Of Bitwise Operator *******\n\n");
+int Govschool =34;
+int LMG = 6;
+printf("Bitwise '^' operator %d\n", Govschool^LMG);
+printf("Bitwise '|' operator %d\n", Govschool|LMG);
+printf("Btiwise '~' operator %d\n", ~Govschool);
+printf("Bitwise '>>' operator %d\n", Govschool>>1);
+printf("Bitwise '<<' operator %d\n", Govschool<<1);
+printf("Bitwise '&' operator %d\n\n", Govschool&LMG);
+printf("******* Taking input form the user *******\n\n");
+int num;
+printf("Enter the the number: ");
+scanf("%d",&num);
+printf("Your inputed number is %d",num);
+    return 0;
+}
+*/
