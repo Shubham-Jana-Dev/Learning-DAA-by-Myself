e5807730ad73330126b39d667e4d9196c1ccc3f1
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-17 05:52:56 +0530

    feat: convert the algorithm for adding two number into program

--- introduction.c
+++ introduction.c
@@ -1,3 +1,14 @@
+// convert the algorithm into a C program to sum of two numbers 
+#include <stdio.h>
+int main(){
+int num1, num2;
+printf("Enter the first number: ");
+scanf("%d",&num1);
+printf("Enter the second number: ");
+scanf("%d",&num2);
+printf("%d + %d = %d",num1,num2,num1+num2);
+    return 0;
+}
 // Calculate a factorial of a number
 /*
 #include <stdio.h>
