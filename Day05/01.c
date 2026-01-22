4072b56154bdf3330b2140d9d66f373bdf337108
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-22 13:35:45 +0530

    feat: delete Unnecessary files

---
+++ Day05/01.c
@@ -0,0 +1,13 @@
+// 📚 Decidable Problems
+
+// The problem for which efficient algorithm exists(at least on).
+// Takes polynomial time or less to execute.
+
+// 📚 Undecidable Problems
+
+// The problem for which no efficient algorithm exists.
+// Takes exponential time to execute.
+
+// Finite Input ----> Algorithm(undecidable) -----> Terminates in exponential time
+
+// 📚 The example plote of Polynomial vs. Exponential Time
