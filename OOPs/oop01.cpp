1d05211aba83e0939d88baf60f4efdd25b6159cb
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-12 06:21:48 +0530

    more optimized way to  writ the inputs

--- OOPs/oop01.cpp
+++ OOPs/oop01.cpp
@@ -3,20 +3,25 @@ using namespace std;
 
 class employee{
     public:
-string name;
-string company;
-int age;
+string Name;
+string Company;
+int Age;
 void IntroduceYourself(){
-cout << "name: "<< name<<endl;
-cout << "Company: "<< company<<endl;
-cout << "Age: "<< age << endl;
+cout << "name: "<< Name<<endl;
+cout << "Company: "<< Company<<endl;
+cout << "Age: "<< Age << "\n" <<endl;
+}
+employee(string name, string company, int age){
+    Name = name;
+    Company = company;
+    Age = age;
 }
 };
 int main(){
-employee employee1;
-employee1.name = "Shubham";
-employee1.company = "Not yet";
-employee1.age = 19;
+employee employee1 = employee("Shubham","Not yet",19);
 employee1.IntroduceYourself();
+
+employee employee2 = employee("Rajat","HCL",23);
+employee2.IntroduceYourself();
     return 0;
 }
