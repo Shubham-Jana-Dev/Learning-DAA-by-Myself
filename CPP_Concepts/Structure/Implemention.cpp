c1cc2b411f687939ccbd9faf89acda91ca532117
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-04-01 09:26:58 +0530

    feat: implement the array into the structure of a group of data

---
+++ CPP_Concepts/Structure/Implemention.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+struct student{
+    string name;
+    int roll;
+    float gpa;
+    bool attendance = true;
+};
+int main(){
+student Student[3] = { 
+{"Shubham",34,7.5},
+{"Neha",32,7.8},
+{"Rajat",17,5.2,false}
+};
+for (int i = 0; i<3; i++){
+    cout << "\n-----" << Student[i].roll << "-----\n";
+    cout << "Hi " << Student[i].name << "!\nYour roll number is " << Student[i].roll << ".\nLast semester you got. " << Student[i].gpa << "." << endl;
+    if (Student[i].attendance){
+        cout << "Great " << Student[i].name << ", You have a 75% + attendance keep it up." << endl;
+    }
+    else{
+        cout << Student[i].name <<", You should work on your attendace." << endl;
+    }
+}
+    return 0;
+}
---
+++ Dynamic-Memory-Allocation/README.md
@@ -0,0 +1,5 @@
+### Dynamic Memory Alocation
+
+The procsee of allocating memory at the time of the execution is called dynamic memory allocation.
+
+<img src="../assets/Dynamic_memory_alocation_hash_map.png" alt="Project Screenshot" width="500">
