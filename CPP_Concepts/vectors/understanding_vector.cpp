d99d3689d48598390708d7e6434b91bfdc3297ea
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-04-02 07:01:16 +0530

    feat:understand how vectors works and know about iteration

--- CPP_Concepts/vectors/understanding_vector.cpp
+++ CPP_Concepts/vectors/understanding_vector.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 void displayv(vector<int> &v){
     for (int i = 0; i < v.size(); i++){
-        cout << v[i] <<" "<<endl;
+        cout << v[i] <<" ";
     }
     cout<<endl;
 }
 int main(){
     vector<int> vec1;
-    int element;
-    for (int i = 0; i < 4; i++){
-        cout<< "Enter the aliments to add to this vector";
+    int element,size;
+    cout << "Enter the size of the vector: ";
+    cin >> size;
+    for (int i = 0; i < size; i++){
+        cout<< "Enter the aliments to add to this vector: ";
         cin>>element;
         vec1.push_back(element);
     }
-displayv(vec1);
+displayv(vec1); 
 }
